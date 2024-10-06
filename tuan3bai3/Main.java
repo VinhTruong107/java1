d37d1735bf0a9baeb1c3319ade4996738c262489
vinh <kimvinh869@gmail.com>
2024-10-06 22:21:26 +0700

    tuan3bai3

---
+++ tuan3bai3/Main.java
@@ -0,0 +1,12 @@
+package tuan3bai3;
+
+public class Main {
+    public static void main(String[] args) {
+        // Tạo đối tượng HinhTron và nhập thông tin
+        HinhTron hinhTron = new HinhTron();
+        hinhTron.nhapBanKinh();
+
+        // Xuất thông tin hình tròn
+        hinhTron.xuatThongTin();
+    }
+}
