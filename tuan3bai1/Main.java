7673e697c9317d6079f3f30b888ed2b8b6750970
vinh <kimvinh869@gmail.com>
2024-10-06 21:42:32 +0700

    Add or update tuan3bai1

---
+++ tuan3bai1/Main.java
@@ -0,0 +1,21 @@
+package tuan3bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of DIEM
+        DIEM diem = new DIEM();
+
+        // Input the point coordinates
+        diem.nhapDiem();
+
+        // Output the point coordinates
+        diem.xuatDiem();
+
+        // Move the point by a given dx, dy
+        diem.diChuyenDiem(5, 10);
+
+        // Output the new point coordinates after moving
+        System.out.println("Sau khi di chuyen:");
+        diem.xuatDiem();
+    }
+}
